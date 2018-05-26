848ddbd43b1e686a87cd94f8ba63870206fffff9
Ram <ramusa2011@ucla.edu>
2018-05-26 08:55:27 -0700

    file added

--- Project2/support.h
+++ Project2/support.h
@@ -16,6 +16,8 @@
 #include <string.h>
 #include <arpa/inet.h>
 
+#include <sys/timeb.h>  // timestamp in millisecond
+
 
 #define MAX_SEQ_NUM 30720
 #define MAX_PKT_LENGTH 1024
@@ -28,6 +30,45 @@
 #define DATA 3
 #define ACK 4
 #define FIN 5
+
+
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+
+//FOR CONGESTION WINDOW
+int ssthreash = 15360;
+int cwnd = 1024;
+int dup_ACK_COUNT = 0;
+
+//PRINT ERROR
+void error(const char* error_message) {
+    perror(error_message);
+    exit(1);
+}
+
+
+//GET SYSTEM TIMESTAMP IN MILLISECONDS
+//ref:https://programmingtoddler.wordpress.com/2014/11/09/c-how-to-get-system-timestamp-in-second-millisecond-and-microsecond/
+long long sys_timestamp() {
+    struct timeb timer_msec;
+    long long int timestamp_msec; /* timestamp in millisecond. */
+    if (!ftime(&timer_msec)) {
+        timestamp_msec = ((long long int) timer_msec.time) * 1000ll +
+        (long long int) timer_msec.millitm;
+    }
+    else {
+        timestamp_msec = -1;
+    }
+    return timestamp_msec;
+}
+
+
+//set congestion window
+void set_cwnd(int value) {
+    cwnd = MIN(value, MAX_SEQ_NUM/2);
+}
+
+
 
 
 
